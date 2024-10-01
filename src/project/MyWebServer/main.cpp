93bea25f30ef2e85bb232761d1dfff975fd2e315
Lightouch9 <arito@foxmail.com>
2024-10-01 23:50:47 +0800

    创建项目的main文件

---
+++ src/project/MyWebServer/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // - 初始化`WebServer`对象`server`
+    // - 启动`server`的日志记录
+    // - 启动`server`的数据库
+    // - 启动`server`的线程池
+    // - 设置`server`的触发模式
+    // - 初始化`server`的事件监听设置
+    // - 启动`server`的核心事件循环
+    return 0;
+}
