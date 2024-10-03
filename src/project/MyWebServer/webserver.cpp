88f50a9d33e9445cbb3e169c09d1f9bce5b7296a
Lightouch9 <arito@foxmail.com>
2024-10-03 23:12:55 +0800

    初步完善事件监听的实现

--- src/project/MyWebServer/webserver.cpp
+++ src/project/MyWebServer/webserver.cpp
@@ -1,13 +1,29 @@
 //webserver.h中各方法的实现
-#include"webserver.h"
+#include "webserver.h"
 
 //构造函数
 WebServer::WebServer(){}
 //析构函数
 WebServer::~WebServer(){}
+//初始化函数
+void WebServer::init(){}
 //线程池的创建与初始化
 void WebServer::thread_pool(){}
 //事件监听的创建与初始化
-void WebServer::eventListen(){}
+void WebServer::eventListen()
+{
+    //创建监听用的网络套接字
+    m_listenfd=socket(PF_INET, SOCK_STREAM, 0);
+    assert(m_listenfd>=0);
+
+    //初始化地址结构
+    int ret=0;
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));   //将address内清空，确保这块内存是干净的
+    address.sin_family=AF_INET; //设置地址结构的协议族
+    address.sin_addr.s_addr=htonl(INADDR_ANY);  //设置绑定本地任意ip
+    address.sin_port=htons(m_port); //绑定端口
+
+}
 //服务器事件循环的开始
 void WebServer::eventLoop(){}
--- src/project/MyWebServer/webserver.h
+++ src/project/MyWebServer/webserver.h
@@ -1,10 +1,13 @@
 //头文件引用重复性检查
 #ifndef WEBSERVER_H
 #define WEBSERVER_H
-
+#include<assert.h>
+#include<netinet/in.h>
 class WebServer
 {
     public:
+        //成员方法
+
         //构造函数与析构函数
         WebServer();
         ~WebServer();
@@ -17,5 +20,10 @@ class WebServer
         void eventListen();
         //服务器事件循环开始
         void eventLoop();
+    
+    public:
+        //成员变量
+        int m_listenfd;   //监听套接字
+        int m_port; //监听套接字的端口
 };
 #endif
