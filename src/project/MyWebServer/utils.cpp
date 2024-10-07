4c26e4bd5490a7b8ac60654656f67b056cbca81f
Lightouch9 <arito@foxmail.com>
2024-10-07 23:31:13 +0800

    信号处理函数的实现

--- src/project/MyWebServer/utils.cpp
+++ src/project/MyWebServer/utils.cpp
@@ -39,3 +39,25 @@ void Utils::addfd(int epollfd, int fd, bool is_one_shot, int trig_mode)
     //设置非阻塞
     setnonblocking(fd);
 }
+//信号处理函数
+void Utils::sig_handler(int sig)
+{
+    
+    int save_errno=errno;   //保证可重入性，保留全局变量errno
+    int msg=sig;
+    send(m_pipefd[1], (char*)&msg, 1, 0); //发送到管道的写端，统一事件源
+    errno=save_errno;
+}
+//设置信号处理函数
+void Utils::addsig(int sig, void(handler)(int), bool restart)
+{
+    struct sigaction sa;    //用于存储对信号的处理方式
+    memset(&sa, '/0', sizeof(sa));  //将此内存重置为0
+    sa.sa_handler=handler;  //指定信号处理函数
+
+    if(restart)
+        sa.sa_flags |= SA_RESTART;  //如果restart为true，则设置中断重启行为
+
+    sigfillset(&sa.sa_mask);    //设置信号掩码，在处理信号sig时，屏蔽所有信号
+    assert(sigaction(sig, &sa, NULL)!=-1);
+}
--- src/project/MyWebServer/utils.h
+++ src/project/MyWebServer/utils.h
@@ -1,5 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include<assert.h>
+#include<stdlib.h>
 //工具类，用于提供一些辅助操作
 class Utils
 {
@@ -13,8 +15,16 @@ class Utils
         int setnonblocking(int fd);
         //向epoll内核事件表中添加事件
         void addfd(int epollfd, int fd, bool is_one_shot, int trig_mode);
-        //设置信号处理行为
+        //信号处理函数
+        void sig_handler(int sig);
+        //设置信号处理函数
         void addsig(int sig, void(handler)(int), bool restart);
+
+    public:
+        //成员变量
+        static int *m_pipefd;   //存储管道
+        static int m_epollfd;   //存储epoll文件描述符
+
 };
 
 
